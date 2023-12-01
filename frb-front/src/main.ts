290df8468f08cff04102d59f376e0ab8a4713750
Alex Bian <2317164114@qq.com>
2023-12-01 15:06:43 +0800

    v 3.0

--- frb-front/src/main.ts
+++ frb-front/src/main.ts
@@ -9,3 +9,14 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
+
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+});
---
+++ js/show-box1.js
@@ -0,0 +1,49 @@
+var dom = document.getElementById('rate-show');
+var myChart1 = echarts.init(dom, null, {
+  renderer: 'canvas',
+  useDirtyRect: false
+});
+var app = {};
+
+var option;
+
+option = {
+  title: {
+    text: '司机应答比率',
+    left: 'center'
+  },
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    orient: 'vertical',
+    left: 'left'
+  },
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: '50%',
+      data: [
+        { value: 1048, name: '滴滴' },
+        { value: 735, name: '神州出行' },
+        { value: 580, name: '高德打车' },
+        { value: 484, name: 'T3出行' },
+        { value: 300, name: '其他' }
+      ],
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }
+  ]
+};
+
+if (option && typeof option === 'object') {
+  myChart1.setOption(option);
+}
+
+window.addEventListener('resize', myChart1.resize);
